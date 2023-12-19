71292b7bc854e701041c910ce65590879ed4bcee
Mike <mikemcguiness@protonmail.com>
2023-12-18 17:57:23 -0800

    Fix 'scrollIntoView' doesn't scroll far enough when first loading data

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
         {selectedCountry &&
           <h3 className="text-5xl text-center font-bold my-12 uppercase">{twoLetterCountryCodeToFullName(selectedCountry)}</h3>
         }
-        <div ref={selectedStoryLocationRef} className='flex flex-wrap justify-center gap-20 pb-20'>
+        <div ref={selectedStoryLocationRef} className='flex flex-wrap justify-center gap-20 pb-20 min-h-[30rem]'>
           {selectedCountryArticles && selectedCountryArticles.length > 0 &&
             selectedCountryArticles.map((country, index) => {
               return (
